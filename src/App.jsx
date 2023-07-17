9d8d89848eef060cca2d146df1fac71e242e6572
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-17 14:46:44 +0200

    controlamos el estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,57 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import responseMovies from "./mocks/with-results.json";
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
 // const API_KEY_OMDB = '6c52a547';
 // const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY_OMDB}&s=${search}`;
 
 function App() {
-  const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
+  const { movies } = useMovies();
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { query } = Object.fromEntries(new window.FormData(event.target));
+    console.log(query);
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    if (query === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (query.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [query]);
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form>
-          <input placeholder="Avengers, matrix..." />
-          <button>Search</button>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            name="query"
+            onChange={handleChange}
+            value={query}
+            placeholder="Avengers, matrix..."
+          />
+          <button type="submit">Search</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
-        <ul className="movies">
-          {hasMovies ? (
-            movies.map((movie) => (
-              <li className="movie" key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <p>{movie.Year}</p>
-                <img src={movie.Poster} alt={movie.imdbID} />
-              </li>
-            ))
-          ) : (
-            <p>No se han encontrado resultados</p>
-          )}
-        </ul>
+        <Movies movies={movies} />
       </main>
     </div>
   );
---
+++ src/services/movies.js
@@ -0,0 +1,5 @@
+export const SearchMovies = ({search}) => {
+  const API_KEY_OMDB = '6c52a547';
+	const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY_OMDB}&s=${search}`;
+
+}
