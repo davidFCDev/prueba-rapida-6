5fe7a942c15b148cbeb3aa5a8e09d45e10683a27
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-17 13:42:31 +0200

    template con mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
+// const API_KEY_OMDB = '6c52a547';
+// const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY_OMDB}&s=${search}`;
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
+
   return (
     <div className="page">
       <header>
@@ -11,7 +17,21 @@ function App() {
         </form>
       </header>
 
-      <main></main>
+      <main>
+        <ul className="movies">
+          {hasMovies ? (
+            movies.map((movie) => (
+              <li className="movie" key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.imdbID} />
+              </li>
+            ))
+          ) : (
+            <p>No se han encontrado resultados</p>
+          )}
+        </ul>
+      </main>
     </div>
   );
 }
