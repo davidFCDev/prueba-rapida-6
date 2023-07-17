3b2e88fee2e5cf0212ec5749f0c683ac03ebf711
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-17 15:00:03 +0200

    añadimos useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
 function useSearch() {
   const [search, updateSearch] = useState("");
   const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    
     if (search === "") {
       setError("No se puede buscar una película vacía");
       return;
